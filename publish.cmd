b92081ac79debc6dae1137ad18e80c401e10e4ff
yns余年胜 <nsyu@ctrip.com>
2018-06-08 16:04:48 +0800

    update the solution structure

--- publish.cmd
+++ publish.cmd
@@ -1,2 +1,2 @@
-dotnet build
+dotnet build -c Release
 dotnet nuget push .\artifacts\Crisp.Extensions.Configuration.*.nupkg -s https://www.nuget.org -k oy2exlynczjpbbgbvfvqj5cwo5qi6cwepnjcmidsp55sya
